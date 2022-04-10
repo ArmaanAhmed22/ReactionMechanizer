820cc8644489e64bd42a86c688181b2560779a0c
Armaan Ahmed <aahmed22@scarsdaleschools.org>
2022-04-09 20:10:06 -0400

    🔧  Added keyword arguments (makes it work)

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 if __name__ == '__main__':
-    setup()
+    setup(
+        packages=find_packages(where="src"),
+        package_dir={"": "src"}
+    )
